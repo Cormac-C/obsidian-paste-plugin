c0aa4653db01d43820d2763fdc5a11778158537b
Cormac Cureton <cormaccureton@gmail.com>
2025-02-02 10:40:24 -0500

    Add clean text command

--- main.ts
+++ main.ts
@@ -28,21 +28,30 @@ export default class BetterPastePlugin extends Plugin {
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon(
 			"dice",
-			"Sample Plugin",
+			"Paste Plugin",
 			(evt: MouseEvent) => {
 				// Called when the user clicks the icon.
 				new Notice("This is a notice!");
 			}
 		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
-		this.addRibbonIcon("dice", "Greet", () => {
-			new Notice("Hello, world!");
+		this.addCommand({
+			id: "paste-cleaned-text",
+			name: "Paste Cleaned Text",
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				await this.pasteCleanedText(editor);
+			},
+			hotkeys: [
+				{
+					modifiers: ["Mod", "Shift"],
+					key: "v",
+				},
+			],
 		});
+		// Look at how paste is handled here: https://github.com/denolehov/obsidian-url-into-selection/blob/master/src/main.ts
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		// this.addSettingTab(new SampleSettingTab(this.app, this));
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
@@ -63,6 +72,22 @@ export default class BetterPastePlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
+	}
+
+	async pasteCleanedText(editor: Editor): Promise<void> {
+		const clipboardText = await navigator.clipboard.readText();
+		const cleanedText = this.cleanText(clipboardText);
+		editor.replaceSelection(cleanedText);
+	}
+
+	cleanText(text: string): string {
+		// TODO: look at making it more customizable
+		const cleanedText = text
+			.replace(/-\s*\n\s*/g, "") // Merge hyphenated words
+			.replace(/(\r\n|\n|\r)/gm, " ") // Replace all newlines with spaces
+			.replace(/\s{2,}/g, " ") // Replace multiple spaces with a single space
+			.trim();
+		return cleanedText;
 	}
 }
 
